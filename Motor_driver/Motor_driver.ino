754abfd9a861f55396466ff2d3087eed2898b4b2
Likfun <148438356+Likfun@users.noreply.github.com>
2023-12-25 22:35:05 +0800

    Update Motor_driver.ino

--- Motor_driver/Motor_driver.ino
+++ Motor_driver/Motor_driver.ino
@@ -6,23 +6,13 @@ int RB = A3;//Right back pin
 int RF = A0;//Right front pin
 int PL = 3;//Enable pin left
 int PR = 2;//Enable pin right
-/*int EN = 10;
-int IR = 11;
-int IL = 12;*/
-
-LiquidCrystal lcd(8,9,4,5,6,7);
 
 void setup() {
-  // put your setup code here, to run once:
 pinMode(LB, OUTPUT);
   pinMode(LF, OUTPUT);
   pinMode(RB, OUTPUT);
   pinMode(RF, OUTPUT);
   pinMode(PR, OUTPUT);
-
-
-    lcd.begin(16, 2);
-    lcd.print("hello");
 }
 
 void loop() {
@@ -32,7 +22,7 @@ void loop() {
     digitalWrite(RB,LOW);
     digitalWrite(LF,HIGH);//turning left forward
     digitalWrite(LB,LOW);
-    delay(1000);
+    delay(1000);//delay for 1000ms
     analogWrite(PR,170);
     analogWrite(PL,170);
     digitalWrite(RF,LOW);
