7c82e847e0a96be6e29d469a156b85a42f2ead78
Likfun <148438356+Likfun@users.noreply.github.com>
2023-12-26 00:01:08 +0800

    Motor_driver.ino

--- Motor_driver/Motor_driver.ino
+++ Motor_driver/Motor_driver.ino
@@ -7,12 +7,16 @@ int RF = A0;//Right front pin
 int PL = 3;//Enable pin left
 int PR = 2;//Enable pin right
 
+
 void setup() {
+  // put your setup code here, to run once:
 pinMode(LB, OUTPUT);
   pinMode(LF, OUTPUT);
   pinMode(RB, OUTPUT);
   pinMode(RF, OUTPUT);
+  pinMode(PL, OUTPUT);
   pinMode(PR, OUTPUT);
+
 }
 
 void loop() {
@@ -22,7 +26,7 @@ void loop() {
     digitalWrite(RB,LOW);
     digitalWrite(LF,HIGH);//turning left forward
     digitalWrite(LB,LOW);
-    delay(1000);//delay for 1000ms
+    delay(1000);
     analogWrite(PR,170);
     analogWrite(PL,170);
     digitalWrite(RF,LOW);
