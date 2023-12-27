45ee5ee69fdc38a88bef6f33b6f9247181e9b335
Likfun <148438356+Likfun@users.noreply.github.com>
2023-12-27 22:12:43 +0800

    Update Ultrasonic_sensor.ino

--- Ultrasonic_sensor.ino
+++ Ultrasonic_sensor.ino
@@ -1,60 +1,52 @@
-#include <LiquidCrystal.h>
+// Include NewPing Library
+#include "NewPing.h"
 
-int LB = A1;//Left back pin
-int LF = A2;//Left front pin
-int RB = A3;//Right back pin
-int RF = A0;//Right front pin
-int PL = 3;//Enable pin left
-int PR = 10;//Enable pin right
-int EN = 2;
+// Hook up HC-SR04 with Trig to Arduino Pin 9, Echo to Arduino pin 10
+#define TRIGGER_PIN 9
+#define ECHO_PIN 10
+
+// Maximum distance we want to ping for (in centimeters).
+#define MAX_DISTANCE 400	
+
+// NewPing setup of pins and maximum distance.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+int LB = A1;
+int LF = A2;
+int RB = A3;
+int RF = A5;
 int IR = 11;
-int IL = 12;
-int distance=0;
-
-LiquidCrystal lcd(8,9,4,5,6,7);
+int IL = 8;
+int PL = 5;
+int PR = 6;
+int EN = 13;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  lcd.begin(16, 2);
-  pinMode(LB, OUTPUT);
-  pinMode(LF, OUTPUT);
-  pinMode(RB, OUTPUT);
-  pinMode(RF, OUTPUT);
-  pinMode(IR, INPUT);
-  pinMode(IL, INPUT);
-  pinMode(PR, OUTPUT);
-  pinMode(PL, OUTPUT);
-  pinMode(EN, INPUT);
+	Serial.begin(9600);
+    {analogWrite(PL,170);
+    analogWrite(PR,170);
+    digitalWrite(RF,HIGH);
+    digitalWrite(RB,LOW);
+    digitalWrite(LF,HIGH);
+    digitalWrite(LB,LOW);}
 }
 
 void loop() {
-    analogWrite(PR,170);//speed of 170
-    analogWrite(PL,170);
-    digitalWrite(RF,LOW);
-    digitalWrite(RB,LOW);
-    digitalWrite(LF,HIGH);//turning left forward
-    digitalWrite(LB,LOW);
-    delay(1000);
+	Serial.print("Distance = ");
+	Serial.print(sonar.ping_cm());
+	Serial.println(" cm");
+  int a=sonar.ping_cm();
+  if(a<35)
+    {analogWrite(PL,170);
     analogWrite(PR,170);
-    analogWrite(PL,170);
-    digitalWrite(RF,LOW);
-    digitalWrite(RB,LOW);
-    digitalWrite(LF,LOW);
-    digitalWrite(LB,LOW);//stop
-    delay(1000);
-    analogWrite(PR,170);
-    analogWrite(PL,170);
-    digitalWrite(RF,HIGH);//turning right forward
-    digitalWrite(RB,LOW);
-    digitalWrite(LF,LOW);
-    digitalWrite(LB,LOW);
-    delay(1000);
-    analogWrite(PR,170);
-    analogWrite(PL,170);
     digitalWrite(RF,LOW);
     digitalWrite(RB,LOW);
     digitalWrite(LF,LOW);
-    digitalWrite(LB,LOW);//stop again
-    delay(1000);
+    digitalWrite(LB,LOW);}
+else
+    {analogWrite(PL,170);
+    analogWrite(PR,170);
+    digitalWrite(RF,HIGH);
+    digitalWrite(RB,LOW);
+    digitalWrite(LF,HIGH);
+    digitalWrite(LB,LOW);}
 }
